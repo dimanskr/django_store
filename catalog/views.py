cd38fbfe91ad14187b3e986d4e076667d3138e96
Dimanskr <dimanskr2@gmail.com>
2024-09-05 18:18:38 +0300

    Add to catalog app urls, views, templates

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        # выводим в консоль информацию о пользователе
+        print(f"Пользователь {name} оставил комментарий '{message}'"
+              f" и просил связаться с ним по телефону {phone} ")
+    return render(request, 'catalog/contacts.html')
