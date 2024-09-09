d9acf0e15bccc173de1c285830be8ac7c384ed60
Dimanskr <dimanskr2@gmail.com>
2024-09-09 18:07:32 +0300

    Create basic models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    """
+    Класс категории товара
+    """
+
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Наименование",
+        help_text="Введите наименование категории",
+    )
+
+    def __str__(self):
+        """
+        Строковое категории
+        """
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
+
+
+class Product(models.Model):
+    """
+    Класс товара
+    """
+
+    name = models.CharField(
+        max_length=100,
+        verbose_name="Товар",
+        help_text="Введите наименование товара",
+    )
+
+    def __str__(self):
+        return f"{self.name}"
+
+    class Meta:
+        verbose_name = "товар"
+        verbose_name_plural = "товары"
