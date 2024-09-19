405787d4af9adb3645814a6afe6fe72203746d61
Dimanskr <dimanskr2@gmail.com>
2024-09-19 20:15:38 +0300

    Add product list

--- catalog/views.py
+++ catalog/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render, get_object_or_404
 from catalog.models import Product
 
 
-def home(request):
-    return render(request, 'catalog/home.html')
+def product_list(request):
+    products = Product.objects.all()
+    context = {"products": products}
+    return render(request, "catalog/product_list.html", context)
 
 
 def product_detail(request, pk):
