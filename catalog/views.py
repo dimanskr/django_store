5f26d69545e7f988427352c71abc931acce2d629
Dimanskr <dimanskr2@gmail.com>
2024-09-19 18:09:24 +0300

    add product detail

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from catalog.models import Product
 
 
 def home(request):
     return render(request, 'catalog/home.html')
+
+
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, "catalog/product_detail.html", context)
 
 
 def contacts(request):
@@ -11,6 +19,6 @@ def contacts(request):
         phone = request.POST.get('phone')
         message = request.POST.get('message')
         # выводим в консоль информацию о пользователе
-        print(f"Пользователь {name} оставил комментарий '{message}'"    
+        print(f"Пользователь {name} оставил комментарий '{message}'"
               f" и просил связаться с ним по телефону {phone} ")
     return render(request, 'catalog/contacts.html')
