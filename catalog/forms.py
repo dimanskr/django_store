4ba6d78cd35f3c1542f2f2967d725621755bb409
Dimanskr <dimanskr2@gmail.com>
2024-10-15 16:52:32 +0300

    add pagination tmpl and bootstrap styling app for forms

--- catalog/forms.py
+++ catalog/forms.py
@@ -2,19 +2,7 @@ from django import forms
 from .models import Product, Version
 
 
-class StyleFormMixin:
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        for field_name, field, in self.fields.items():
-            if isinstance(field, forms.BooleanField):
-                field.widget.attrs['class'] = 'form-check-input'
-            elif isinstance(field, forms.DateTimeField):
-                field.widget.attrs['class'] = 'form-control datetimepicker'
-            else:
-                field.widget.attrs['class'] = 'form-control'
-
-
-class ProductForm(StyleFormMixin, forms.ModelForm):
+class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = "__all__"
@@ -41,7 +29,7 @@ class ProductForm(StyleFormMixin, forms.ModelForm):
         return cleaned_data
 
 
-class VersionForm(StyleFormMixin, forms.ModelForm):
+class VersionForm(forms.ModelForm):
     class Meta:
         model = Version
         fields = "__all__"
